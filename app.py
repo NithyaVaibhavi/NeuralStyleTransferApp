f818df0aba8dbc1f5bc4921af66e83d10ecd7327
vnv3002@gmail.com <vnv3002@gmail.com>
2024-11-03 16:25:47 +0530

    changed the app.py root element part 2

--- app.py
+++ app.py
@@ -107,4 +107,5 @@ def multi_style_transfer_handler():
     return jsonify({'resultImageUrls': [f'/static/output/stylized_image.jpg']})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if not set
+    app.run(host='0.0.0.0', port=port, debug=True)
